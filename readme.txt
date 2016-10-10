b4832644b55f80cd220598ca0caa107a4e7e56fb
Qiaoyu Deng <qdeng@andrew.cmu.edu>
2016-10-10 00:17:59 -0400

    Start fight for AutoLab

---
+++ readme.txt
@@ -0,0 +1,1 @@
+a basic https server, lisod
---
+++ tar-cp3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+dire="15-441-project-1"
+if [ -d "$dire" ]; then
+  rm -r "$dire"
+  mkdir "$dire"
+else
+  mkdir "$dire"
+fi
+
+tarfile="15-441-project-1-cp3.tar"
+if [ -f "$tarfile" ]; then
+  rm "$tarfile"
+fi
+
+cp lisod.c "$dire"
+cp lisod.h "$dire"
+cp log.c "$dire"
+cp log.h "$dire"
+cp cgi_func.c "$dire"
+cp dbg_func.c "$dire"
+cp dbg_func.h "$dire"
+cp lexer.l "$dire"
+cp parser.y "$dire"
+cp parse.c "$dire"
+cp time_gmt.c "$dire"
+cp daemonize.c "$dire"
+cp Makefile "$dire"
+cp -r .git "$dire"
+tar -cvf "$tarfile" "$dire"
